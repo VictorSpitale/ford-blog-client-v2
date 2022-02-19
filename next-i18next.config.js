ccdd497fd3664fa53f6895aba5d1103d33214990
Victor SPITALE <v.spitale@groupe-aen.info>
2022-02-19 10:19:08 +0100

    Setting up translations

---
+++ next-i18next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	i18n: {
+		defaultLocale: 'fr',
+		locales: ['en-US', 'fr'],
+		localeDetection: false,
+	},
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import SEO from "../components/seo";
+import {useTranslation} from "next-i18next";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 
 export default function Home() {
+    const {t} = useTranslation()
     return (
         <>
             <SEO title={"Accueil"} />
-            <h1>Titre</h1>
+            <h1>{t('welcome')}</h1>
             <p>Texte</p>
         </>
     );
 }
+
+export const getServerSideProps = async ({locale}: { locale: string }) => ({
+    props: {
+        ...(await serverSideTranslations(locale, ['common']))
+    }
+});
