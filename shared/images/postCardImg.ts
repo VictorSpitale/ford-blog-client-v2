b93ded5f136ed4b394fde5be891667fb871b0c07
Victor SPITALE <v.spitale@groupe-aen.info>
2022-03-05 11:36:24 +0100

    Fix card img

--- shared/images/postCardImg.ts
+++ shared/images/postCardImg.ts
@@ -3,7 +3,7 @@ import {IPost} from "../types/post.type";
 import pic from '../../tests/stub/ford-f-100-eluminator-concept.jpg'
 
 export const getPostCardImg = (post: IPost): string => {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NEXT_PUBLIC_ENV !== "development") {
         return post.picture
     }
     return pic.src
