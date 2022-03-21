836e142eccc14655519b4382162ef03aba2484b4
Victor SPITALE <v.spitale@groupe-aen.info>
2022-03-21 19:25:24 +0100

    AppContext to track logged user

---
+++ context/AppContext.tsx
@@ -0,0 +1,33 @@
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import {Children} from "../shared/types/props.type";
+import axios from "axios";
+
+export const AppContext = createContext('');
+
+export function AppWrapper({children}: { children: Children }) {
+    const [uuid, setUuid] = useState('');
+
+    useEffect(() => {
+        const fetchUuid = async () => {
+            const token = localStorage.getItem('token');
+            await axios.get(process.env.NEXT_PUBLIC_API_URL + '/auth/jwt', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }).then((res) => {
+                if (res.data) setUuid(res.data)
+            })
+        };
+        fetchUuid();
+    }, [])
+
+    return (
+        <AppContext.Provider value={uuid}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export function useAppContext() {
+    return useContext(AppContext);
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import {AppProps} from "next/app";
 import {wrapper} from "../context/store";
 import {appWithTranslation} from "next-i18next";
 import Layout from "../components/layouts/Layout";
+import {AppWrapper} from "../context/AppContext";
 
 function MyApp({Component, pageProps}: AppProps) {
-    return (<Layout>
-            <Component {...pageProps} />
-        </Layout>
+    return (
+        <AppWrapper>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </AppWrapper>
     )
 }
 
