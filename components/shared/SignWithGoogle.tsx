2b1a71cf30fc9233c30560f1e3a2385eba67062c
Victor SPITALE <v.spitale@groupe-aen.info>
2022-03-25 21:57:57 +0100

    SignWith component (not functional)

---
+++ components/shared/SignWithGoogle.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/SignWith.module.css'
+
+const SignWithGoogle = () => {
+    return (
+
+        <button type="button" className={styles.loginWithGoogleBtn}>
+            Sign in with Google
+        </button>
+    );
+};
+
+export default SignWithGoogle;
