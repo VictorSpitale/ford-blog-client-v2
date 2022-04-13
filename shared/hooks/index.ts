026eec240757d18c7e9750c0074efb3d15107395
Victor SPITALE <v.spitale@groupe-aen.info>
2022-04-13 22:45:56 +0200

    refactor: useFetch hook and add useClickOutside for modal component

---
+++ shared/hooks/index.ts
@@ -0,0 +1,11 @@
+import {useFetch} from "./useFetch";
+import {useTranslation} from "./useTranslation";
+import {useOnClickOutside} from "./useClickOutside";
+import {useModal} from "./useModal";
+
+export {
+    useFetch,
+    useTranslation,
+    useOnClickOutside,
+    useModal
+}
