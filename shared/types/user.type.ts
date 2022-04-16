5aa5b5dd19a2934ec9aef7df7d41b6efb6a37c56
Victor SPITALE <v.spitale@groupe-aen.info>
2022-04-16 16:00:35 +0200

    refactor: user profile picture

--- shared/types/user.type.ts
+++ shared/types/user.type.ts
@@ -3,6 +3,7 @@ export type IUser = {
     pseudo: string;
     email: string;
     role: IUserRole;
+    picture?: string;
     updatedAt: string;
     createdAt: string;
 }
@@ -11,4 +12,11 @@ export enum IUserRole {
     USER = '0',
     POSTER = '1',
     ADMIN = '2',
+}
+
+import defaultSrc from '../../public/static/img/default-profile.png'
+
+export const getUserPictureSrc = (user: IUser) => {
+    if (user.picture) return {src: user.picture, default: false}
+    return {src: defaultSrc.src, default: true};
 }
