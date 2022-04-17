b9ad7e57537ecb7c6af0de76799816418d522c5e
Victor SPITALE <v.spitale@groupe-aen.info>
2022-04-17 18:00:16 +0200

    feat: user account security view translations

--- components/account/SecurityView.tsx
+++ components/account/SecurityView.tsx
@@ -19,15 +19,15 @@ const SecurityView = () => {
         setError('');
         setSuccess('');
         if (password.trim() === "" || password.length < 6) {
-            return setError('Le mot de passe doit contenir au moins 6 caractères');
+            return setError(t.account.security.errors.password);
         }
         await dispatch(updateLoggedUser({password, _id: user._id})).then((res) => {
             if (res.meta.requestStatus === "rejected") {
-                return setError('Erreur lors du changement de mot de passe');
+                return setError(t.account.security.errors.rejectedPassword);
             }
             setPassword('');
             if (ref.current) ref.current.value = '';
-            setSuccess("Votre mot de passe a bien été modifié")
+            setSuccess(t.account.security.success)
         })
     }
 
@@ -36,36 +36,34 @@ const SecurityView = () => {
         setSuccess('');
         await dispatch(deleteAccount(user._id)).then((res) => {
             if (res.meta.requestStatus === "rejected") {
-                return setError("Erreur lors de la suppression du compte")
+                return setError(t.account.security.errors.deleteAccount)
             }
         })
     }
 
     return (
         <BaseView>
-            <h1 className={"text-2xl font-semibold mb-4"}>Sécurité</h1>
+            <h1 className={"text-2xl font-semibold mb-4"}>{t.account.security.title}</h1>
             <p className={"mb-4 rounded w-full bg-red-400 text-white px-3 text-justify"}>{error}</p>
             <p className={"mb-4 rounded w-full bg-green-400 text-white px-3 text-justify"}>{success}</p>
             <form onSubmit={handleSubmit}>
                 <div className={"hidden"}>
                     <input autoComplete={"email"} value={user.email} readOnly={true} />
                 </div>
-                <InputField name={"password"} label={"Mot de passe"} type={"password"}
+                <InputField name={"password"} label={t.account.security.password} type={"password"}
                             autoComplete={"new-password"} ref={ref}
                             onChange={(e) => setPassword(e.target.value)} />
                 <div className={"flex justify-end"}>
                     <button type={"submit"}
-                            className={"mt-5 rounded text-white bg-primary-400 px-3 py-1"}>{pending ? t.common.loading : "Sauvegarder"}
+                            className={"mt-5 rounded text-white bg-primary-400 px-3 py-1"}>{pending ? t.common.loading : t.common.save}
                     </button>
                 </div>
             </form>
             <hr className={"my-3"} />
-            <h1 className={"text-2xl text-red-500"}>Suppression de compte</h1>
-            <p>Attention : Vous ne pourrez pas revenir en arrière. Toutes vos données seront supprimées
-                instantanément.</p>
+            <h1 className={"text-2xl text-red-500"}>{t.account.security.delete.title}</h1>
+            <p>{t.account.security.delete.warning}</p>
             <button onClick={handleDelete}
-                    className={"mt-3 rounded text-red-500 border px-2 shadow hover:bg-red-500 hover:text-white"}>Supprimer
-                mon compte
+                    className={"mt-3 rounded text-red-500 border px-2 shadow hover:bg-red-500 hover:text-white"}>{t.account.security.delete.action}
             </button>
         </BaseView>
     );
