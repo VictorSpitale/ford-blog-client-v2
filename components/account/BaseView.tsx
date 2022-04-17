20b59f3c16b7bca8ce0019538050a17abcf6e3cf
Victor SPITALE <v.spitale@groupe-aen.info>
2022-04-17 18:39:07 +0200

    fix: set max height to account base view

--- components/account/BaseView.tsx
+++ components/account/BaseView.tsx
@@ -3,7 +3,8 @@ import {Children} from "../../shared/types/props.type";
 
 const BaseView = ({children}: { children: Children }) => {
     return (
-        <div className={"bg-neutral-50 w-full p-5 rounded-2xl drop-shadow h-fit mt-4 md:mt-0"}>
+        <div
+            className={"c-scroll bg-neutral-50 w-full p-5 rounded-2xl drop-shadow h-fit mt-4 md:mt-0 max-h-[600px] overflow-y-auto"}>
             {children}
         </div>
     );
