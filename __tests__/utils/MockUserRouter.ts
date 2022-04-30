e3ad3c2cd3d6133834a51443cc1988539e269809
Victor SPITALE <v.spitale@groupe-aen.info>
2022-05-01 00:30:31 +0200

    test: tests utils

---
+++ __tests__/utils/MockUserRouter.ts
@@ -0,0 +1,30 @@
+import {NextRouter} from "next/router";
+
+export function MockUserRouter(router: Partial<NextRouter>): NextRouter {
+    return {
+        basePath: "",
+        pathname: "/",
+        route: "/",
+        query: {},
+        asPath: "/",
+        back: jest.fn(),
+        beforePopState: jest.fn(),
+        prefetch: jest.fn(),
+        push: jest.fn().mockResolvedValue(true),
+        reload: jest.fn(),
+        replace: jest.fn(),
+        events: {
+            on: jest.fn(),
+            off: jest.fn(),
+            emit: jest.fn(),
+        },
+        isFallback: false,
+        isLocaleDomain: false,
+        isReady: true,
+        defaultLocale: 'fr',
+        locale: 'fr',
+        domainLocales: [],
+        isPreview: false,
+        ...router
+    }
+}
