1b165ee404f604879d3d946d24202d760c1b3714
Victor SPITALE <v.spitale@groupe-aen.info>
2022-05-07 21:48:25 +0200

    feat: shared button component

---
+++ components/shared/Button.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {AnyFunction} from "../../shared/types/props.type";
+import {className} from "../../shared/utils/class.utils";
+import Link from "next/link";
+
+type BaseProps = {
+    text: string;
+    classes?: string;
+    type?: "button" | "submit" | "reset" | undefined
+}
+
+type ButtonProps = { element: "button"; onClick?: AnyFunction, type?: string } | { element: "link"; onClick: string }
+
+type PropsType = BaseProps & ButtonProps;
+
+const Button = ({element, onClick, text, classes, type}: PropsType) => {
+
+    const buttonStyle = className(classes || '', "bg-primary-400 text-white px-2 py-1 rounded-lg shadow-md" +
+        " shadow-primary-300/40 hover:shadow-primary-300/60 hover:bg-primary-500")
+
+    return (
+        <>
+            {element === "button" ?
+                <button className={buttonStyle} onClick={onClick} type={type}>
+                    {text}
+                </button>
+                : element === "link" ?
+                    <Link href={onClick}>
+                        <a className={buttonStyle}>{text}</a>
+                    </Link>
+                    : null
+            }
+        </>
+    )
+};
+
+export default Button;
