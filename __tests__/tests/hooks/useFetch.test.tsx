e81a4a3ce59c515047fa3dfcce8c5a378f6aa4b5
Victor SPITALE <v.spitale@groupe-aen.info>
2022-05-07 21:47:21 +0200

    test: change the default error message useFetch

--- __tests__/tests/hooks/useFetch.test.tsx
+++ __tests__/tests/hooks/useFetch.test.tsx
@@ -1,18 +1,28 @@
 import {PostStub} from "../../stub/PostStub";
 import axios from "axios";
-import {useFetch} from "../../../shared/hooks";
+import {useFetch, useTranslation} from "../../../shared/hooks";
 import {IMethods} from "../../../shared/types/methods.type";
 import {act, render} from "@testing-library/react";
 import {TestHook} from "../../utils/TestHook";
 import {AnyFunction} from "../../../shared/types/props.type";
+import {RouterContext} from "next/dist/shared/lib/router-context";
+import {MockUseRouter} from "../../utils/MockUseRouter";
+import fr from '../../../public/static/locales/fr.json'
 
 jest.mock('axios');
+jest.mock('../../../shared/hooks', () => {
+    const original = jest.requireActual('../../../shared/hooks');
+    return {
+        __esModule: true,
+        ...original,
+        useTranslation: jest.fn()
+    }
+})
 
 describe('Use Fetch', function () {
 
     const OLD_ENV = process.env;
     beforeEach(() => {
-
         jest.resetModules();
         process.env = {...OLD_ENV};
     })
@@ -27,12 +37,16 @@ describe('Use Fetch', function () {
         const posts = [PostStub()];
         const res = {data: posts};
         (axios as unknown as jest.Mock).mockResolvedValue(res);
+        (useTranslation as jest.Mock).mockReturnValue(fr);
         let result;
-        render(<TestHook callback={() => {
-            fetchObj = useFetch('/posts', IMethods.GET, (res) => {
-                result = res
-            });
-        }} />)
+        render(
+            <RouterContext.Provider value={MockUseRouter({locale: "fr"})}>
+                <TestHook callback={() => {
+                    fetchObj = useFetch('/posts', IMethods.GET, (res) => {
+                        result = res
+                    });
+                }} />
+            </RouterContext.Provider>)
         expect(fetchObj.loading).toBe(false);
         await act(async () => await fetchObj.load());
         expect(axios).toHaveBeenCalledWith({
@@ -52,9 +66,14 @@ describe('Use Fetch', function () {
     it('should fetch without callback', async () => {
         process.env.NEXT_PUBLIC_API_URL = "http://localhost:5000";
         (axios as unknown as jest.Mock).mockResolvedValue(null);
-        render(<TestHook callback={() => {
-            fetchObj = useFetch('/posts', IMethods.GET);
-        }} />)
+        (useTranslation as jest.Mock).mockReturnValue(fr);
+        render(
+            <RouterContext.Provider value={MockUseRouter({locale: "fr"})}>
+                <TestHook callback={() => {
+                    fetchObj = useFetch('/posts', IMethods.GET);
+                }} />
+            </RouterContext.Provider>
+        )
         expect(axios).toHaveBeenCalledWith({
             data: undefined,
             method: IMethods.GET,
@@ -67,12 +86,17 @@ describe('Use Fetch', function () {
         process.env.NEXT_PUBLIC_API_URL = "http://localhost:5000";
         const res = {response: {data: {message: 'Post not found', statusCode: 404, code: 5}}};
         (axios as unknown as jest.Mock).mockRejectedValue(res);
+        (useTranslation as jest.Mock).mockReturnValue(fr);
         let result;
-        render(<TestHook callback={() => {
-            fetchObj = useFetch('/posts', IMethods.GET, (res) => {
-                result = res
-            });
-        }} />)
+        render(
+            <RouterContext.Provider value={MockUseRouter({locale: "fr"})}>
+                <TestHook callback={() => {
+                    fetchObj = useFetch('/posts', IMethods.GET, (res) => {
+                        result = res
+                    });
+                }} />
+            </RouterContext.Provider>
+        )
         expect(fetchObj.loading).toBe(false);
         await act(async () => await fetchObj.load());
         expect(result).not.toBeDefined();
@@ -93,9 +117,13 @@ describe('Use Fetch', function () {
         process.env.NEXT_PUBLIC_API_URL = "http://localhost:5000";
         const res = {response: {data: {error: 'Post not found'}}};
         (axios as unknown as jest.Mock).mockRejectedValue(res);
-        render(<TestHook callback={() => {
-            fetchObj = useFetch('/posts');
-        }} />)
+        (useTranslation as jest.Mock).mockReturnValue(fr);
+        render(
+            <RouterContext.Provider value={MockUseRouter({locale: "fr"})}>
+                <TestHook callback={() => {
+                    fetchObj = useFetch('/posts');
+                }} />
+            </RouterContext.Provider>)
         await act(async () => await fetchObj.load());
         expect(axios).toHaveBeenCalledWith({
             data: undefined,
@@ -103,7 +131,7 @@ describe('Use Fetch', function () {
             url: `${process.env.NEXT_PUBLIC_API_URL}/posts`,
             withCredentials: true
         });
-        expect(fetchObj.error).toBe("Erreur")
+        expect(fetchObj.error).toBe(fr.common.errorSub)
         expect(fetchObj.code).not.toBeDefined();
     })
 
