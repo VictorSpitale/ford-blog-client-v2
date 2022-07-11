f873be260084925cf93488e6551d0b1e4f21567b
Victor SPITALE <v.spitale@groupe-aen.info>
2022-07-11 15:12:18 +0200

    test: chore: remove posts tests from ignored tests

--- jest.config.js
+++ jest.config.js
@@ -38,7 +38,6 @@ module.exports = {
 		'<rootDir>/__tests__/stub/',
 		'<rootDir>/__tests__/mocks/',
 		'<rootDir>/__tests__/utils/',
-		'<rootDir>/__tests__/tests/components/posts'
 	],
 	testEnvironment: 'jsdom',
 	transform: {
