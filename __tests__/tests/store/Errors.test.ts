71818a38b5e8b5d8044ead034e8456e71e86beb4
Victor SPITALE <v.spitale@groupe-aen.info>
2022-07-12 11:34:21 +0200

    test: Store Errors.test.ts

---
+++ __tests__/tests/store/Errors.test.ts
@@ -0,0 +1,34 @@
+import {errorsReducer, ErrorState, ErrorStateType} from "../../../context/reducers/errors.reducer";
+import {AnyAction} from "@reduxjs/toolkit";
+import {setError} from "../../../context/actions/errors.actions";
+
+describe('Errors Actions & Reducers', function () {
+
+    const initialState: ErrorState = {
+        securityViewError: "",
+        profileViewError: ""
+    }
+
+    it('should return the initial state', function () {
+        expect(errorsReducer(undefined, {} as AnyAction)).toEqual(initialState);
+    });
+
+    describe('setError', function () {
+
+        it('should set error', function () {
+
+            const action: AnyAction = {
+                type: setError,
+                payload: {key: "profileViewError", error: "erreur"} as ErrorStateType
+            }
+            const state = errorsReducer(initialState, action);
+            expect(state).toEqual({
+                securityViewError: "",
+                profileViewError: "erreur"
+            })
+
+        });
+
+    });
+
+});
--- context/actions/errors.actions.ts
+++ context/actions/errors.actions.ts
@@ -1,6 +1,6 @@
 import {createAction} from "@reduxjs/toolkit";
-import {ErrorState} from "../reducers/errors.reducer";
+import {ErrorStateType} from "../reducers/errors.reducer";
 
 export const SET_ERROR = "SET_ERROR";
 
-export const setError = createAction<{ key: keyof ErrorState, error: string }>(SET_ERROR);
+export const setError = createAction<ErrorStateType>(SET_ERROR);
