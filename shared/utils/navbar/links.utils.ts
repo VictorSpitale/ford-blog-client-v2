3e2e19d7eff9a42c9ef00671b338eec1c1c9bdb7
Victor SPITALE <v.spitale@groupe-aen.info>
2022-07-17 22:06:41 +0200

    feat: admin link

--- shared/utils/navbar/links.utils.ts
+++ shared/utils/navbar/links.utils.ts
@@ -6,23 +6,29 @@ export type Link = {
     role?: IUserRole
 }
 
-export const links: Link[] = [{
-    code: "0",
-    href: "/"
-}, {
-    code: "1",
-    href: "/news"
-}, {
-    code: "2",
-    href: "/categories"
-}, {
-    code: "3",
-    href: "/write",
-    role: IUserRole.POSTER
-}, {
-    code: "4",
-    href: "/contact"
-}, {
-    code: "5",
-    href: "/account"
-}]
+export const links: Link[] = [
+    {
+        code: "0",
+        href: "/"
+    }, {
+        code: "1",
+        href: "/news"
+    }, {
+        code: "2",
+        href: "/categories"
+    }, {
+        code: "3",
+        href: "/write",
+        role: IUserRole.POSTER
+    }, {
+        code: "4",
+        href: "/contact"
+    }, {
+        code: "5",
+        href: "/account"
+    }, {
+        code: "6",
+        href: "/admin",
+        role: IUserRole.ADMIN
+    }
+]
