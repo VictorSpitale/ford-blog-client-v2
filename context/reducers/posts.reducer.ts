d92c01973c66a6f9d2c4aa4a6156446a90d6b77c
Victor SPITALE <v.spitale@groupe-aen.info>
2022-07-18 20:30:49 +0200

    refactor: handle update / delete posts on admin views

--- context/reducers/posts.reducer.ts
+++ context/reducers/posts.reducer.ts
@@ -1,5 +1,5 @@
 import {IPaginatedPosts} from "../../shared/types/post.type";
-import {getPosts} from "../actions/posts.actions";
+import {deletePost, getPosts, updatePost} from "../actions/posts.actions";
 import {createReducer} from "@reduxjs/toolkit";
 import _ from 'lodash';
 
@@ -20,9 +20,9 @@ const initial: PostsState = {
 }
 export const postsReducer = createReducer(initial, (builder) => {
     builder.addCase(getPosts.pending, (state) => {
-        state.pending = true
+        state.pending = true;
     }).addCase(getPosts.fulfilled, (state, {payload}) => {
-        state.pending = false
+        state.pending = false;
         const unfiltered = [...state.paginatedPosts.posts, ...payload.posts];
         const uniquePosts = _.uniqBy(unfiltered, "_id");
         state.paginatedPosts = {
@@ -31,7 +31,27 @@ export const postsReducer = createReducer(initial, (builder) => {
             page: parseInt(String(payload.page))
         }
     }).addCase(getPosts.rejected, (state) => {
-        state.pending = false
+        state.pending = false;
+    }).addCase(updatePost.pending, (state) => {
+        state.pending = true;
+    }).addCase(updatePost.rejected, (state) => {
+        state.pending = false;
+    }).addCase(updatePost.fulfilled, (state, {payload}) => {
+        const post = state.paginatedPosts.posts.find((p) => p._id === payload._id);
+        const list = state.paginatedPosts.posts;
+        if (post) {
+            state.paginatedPosts.posts = list.map((p) => {
+                if (p._id === payload._id) return payload
+                return p;
+            })
+        }
+    }).addCase(deletePost.pending, (state) => {
+        state.pending = true;
+    }).addCase(deletePost.rejected, (state) => {
+        state.pending = false;
+    }).addCase(deletePost.fulfilled, (state, {payload}) => {
+        state.pending = false;
+        state.paginatedPosts.posts = state.paginatedPosts.posts.filter((p) => p.slug !== payload);
     })
 })
 
