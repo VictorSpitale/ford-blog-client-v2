a1fb53b11ba73c3c191cf51309c3548d5385e986
Victor SPITALE <v.spitale@groupe-aen.info>
2022-07-18 13:43:33 +0200

    refactor: button style

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -3,20 +3,24 @@ import {AnyFunction} from "../../shared/types/props.type";
 import {className} from "../../shared/utils/class.utils";
 import Link from "next/link";
 
-type BaseProps = {
+export type ButtonsProps = {
     text: string;
     classes?: string;
-    type?: "button" | "submit" | "reset" | undefined
+    type?: "button" | "submit" | "reset" | undefined;
+    style?: "primary" | "danger"
 }
 
-type ButtonProps = { element: "button"; onClick?: AnyFunction, type?: string } | { element: "link"; onClick: string }
+type ElementProps = { element: "button"; onClick?: AnyFunction, type?: string } | { element: "link"; onClick: string }
 
-type PropsType = BaseProps & ButtonProps;
+type PropsType = ButtonsProps & ElementProps;
 
-const Button = ({element, onClick, text, classes, type}: PropsType) => {
+const Button = ({element, onClick, text, classes, type, style = "primary"}: PropsType) => {
 
-    const buttonStyle = className(classes || '', "bg-primary-400 text-white px-2 py-1 rounded-lg shadow-md" +
-        " shadow-primary-300/40 hover:shadow-primary-300/60 hover:bg-primary-500")
+    const buttonStyle = className(
+        style === "primary" ? "bg-primary-400 shadow-primary-300/40 hover:shadow-primary-300/60 hover:bg-primary-500" : '',
+        style === "danger" ? "bg-red-400 shadow-red-300/40 hover:shadow-red-300/60 hover:bg-red-500" : '',
+        "text-white px-2 py-1 rounded-lg shadow-md",
+        classes || '')
 
     return (
         <>
