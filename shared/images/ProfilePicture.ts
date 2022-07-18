c4f1cf6b62b69793b69827039320bfc8dba1a200
Victor SPITALE <v.spitale@groupe-aen.info>
2022-07-18 22:14:06 +0200

    refactor: accept basic user type

--- shared/images/ProfilePicture.ts
+++ shared/images/ProfilePicture.ts
@@ -1,7 +1,7 @@
 import defaultSrc from '../../public/static/img/default-profile.png'
-import {IUser} from "../types/user.type";
+import {IBasicUser, IUser} from "../types/user.type";
 
-export const getUserPictureSrc = (user: IUser) => {
+export const getUserPictureSrc = (user: IUser | IBasicUser) => {
     if (process.env.NEXT_PUBLIC_ENV === "development" || !user.picture) return {src: defaultSrc.src, default: true}
     return {src: user.picture, default: false}
 }
