dc35d5ad75370e22ceee2cff2e03a55e1ecfc5a3
Victor SPITALE <v.spitale@groupe-aen.info>
2022-07-20 12:12:53 +0200

    feat: convert role class to display string

--- shared/types/user.type.ts
+++ shared/types/user.type.ts
@@ -1,3 +1,5 @@
+import {Translation} from "../hooks/useTranslation";
+
 export type IUser = {
     _id: string;
     pseudo: string;
@@ -20,6 +22,19 @@ export enum IUserRole {
     USER = '0',
     POSTER = '1',
     ADMIN = '2',
+}
+
+export const displayRole = (role: IUserRole, t: Translation) => {
+    switch (role) {
+        case IUserRole.POSTER:
+            return t.common.roles.poster
+        case IUserRole.USER:
+            return t.common.roles.user
+        case IUserRole.ADMIN:
+            return t.common.roles.admin
+        default:
+            return t.common.roles.user
+    }
 }
 
 
