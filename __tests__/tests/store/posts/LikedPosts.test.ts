ccabc456cfb39617b13ef6c8fc18d30a9502d639
Victor SPITALE <v.spitale@groupe-aen.info>
2022-07-22 11:34:35 +0200

    test: LikedPosts.test.ts

--- __tests__/tests/store/posts/LikedPosts.test.ts
+++ __tests__/tests/store/posts/LikedPosts.test.ts
@@ -1,9 +1,15 @@
 import {likedPostsReducer, LikedPostsState} from "../../../../context/reducers/posts/likedPosts.reducer";
 import {AnyAction} from "@reduxjs/toolkit";
-import {cleanLikedPosts, getLikedPosts} from "../../../../context/actions/posts/posts.actions";
+import {cleanLikedPosts, deletePost, getLikedPosts} from "../../../../context/actions/posts/posts.actions";
 import {PostStub} from "../../../stub/PostStub";
 import * as fetch from "../../../../context/instance";
 import {makeStore} from "../../../../context/store";
+import {deleteCategory, updateCategory} from "../../../../context/actions/categories/categories.actions";
+import {CategoryStub} from "../../../stub/CategoryStub";
+import {deleteAccount, removePicture, updateUser, uploadPicture} from "../../../../context/actions/users/user.actions";
+import {UserStub} from "../../../stub/UserStub";
+import {IUserRole} from "../../../../shared/types/user.type";
+import {CommentStub} from "../../../stub/CommentStub";
 
 describe('LikedPosts Actions & Reducers', function () {
 
@@ -45,7 +51,7 @@ describe('LikedPosts Actions & Reducers', function () {
             })
         });
 
-        it('should set pending false and set posts', function () {
+        it('should set posts if user is not in the list yet', function () {
             const action: AnyAction = {
                 type: getLikedPosts.fulfilled,
                 payload: {posts: [PostStub()], userId: "id"}
@@ -58,12 +64,37 @@ describe('LikedPosts Actions & Reducers', function () {
             })
         });
 
+        it('should replace the posts if user is already in the list', function () {
+            const action: AnyAction = {
+                type: getLikedPosts.fulfilled,
+                payload: {posts: [PostStub()], userId: "01"}
+            }
+            const state = likedPostsReducer({
+                ...initialState,
+                pending: true,
+                users: [{posts: [], userId: "01"}, {posts: [PostStub()], userId: "02"}]
+            }, action);
+            expect(state).toEqual({
+                ...initialState,
+                pending: false,
+                users: [{posts: [PostStub()], userId: "01"}, {posts: [PostStub()], userId: "02"}]
+            })
+        });
+
         it('should set pending false and set posts (action)', async function () {
             const spy = jest.spyOn(fetch, "fetchApi").mockResolvedValue({data: [PostStub()]})
-
             const store = makeStore();
             await store.dispatch(getLikedPosts("user id"));
             expect(spy).toHaveBeenCalled();
+            expect(store.getState().likedPosts.users).toEqual([{posts: [PostStub()], userId: "user id"}]);
+        });
+
+        it('should return the cached posts', async function () {
+            const spy = jest.spyOn(fetch, "fetchApi").mockResolvedValue({data: [PostStub()]})
+            const store = makeStore();
+            await store.dispatch(getLikedPosts("user id"));
+            await store.dispatch(getLikedPosts("user id"));
+            expect(spy).toHaveBeenCalledTimes(1);
             expect(store.getState().likedPosts.users).toEqual([{posts: [PostStub()], userId: "user id"}]);
         });
 
@@ -80,6 +111,262 @@ describe('LikedPosts Actions & Reducers', function () {
             expect(state).toEqual({
                 ...initialState,
                 users: []
+            })
+        });
+
+    });
+
+    describe('UpdateCategory', function () {
+
+        it('should update posts on update category', function () {
+            const action: AnyAction = {type: updateCategory.fulfilled, payload: CategoryStub("gt", "01")}
+            const state = likedPostsReducer({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {...PostStub(), categories: [CategoryStub(), CategoryStub("sport", "01")]},
+                        ], userId: "11"
+                    }
+                ]
+            }, action)
+            expect(state).toEqual({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {...PostStub(), categories: [CategoryStub(), CategoryStub("gt", "01")]},
+                        ], userId: "11"
+                    }
+                ]
+            })
+        });
+
+    });
+
+    describe('DeleteCategory', function () {
+
+        it('should update posts on delete category', function () {
+            const action: AnyAction = {type: deleteCategory.fulfilled, payload: CategoryStub("gt", "01")}
+            const state = likedPostsReducer({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {...PostStub(), categories: [CategoryStub(), CategoryStub("gt", "01")]},
+                        ], userId: "11"
+                    }
+                ]
+            }, action)
+            expect(state).toEqual({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {...PostStub(), categories: [CategoryStub()]},
+                        ], userId: "11"
+                    }
+                ]
+            })
+        });
+
+    });
+
+    describe('DeletePost', function () {
+
+        it('should update post list on delete post', function () {
+            const action: AnyAction = {type: deletePost.fulfilled, payload: "slug"}
+            const state = likedPostsReducer({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            PostStub(),
+                            {...PostStub(), slug: "slug"}
+                        ], userId: "11"
+                    }
+                ]
+            }, action)
+            expect(state).toEqual({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            PostStub()
+                        ], userId: "11"
+                    }
+                ]
+            })
+        });
+
+    });
+
+    describe('removePicture', function () {
+
+        it('should update the posts comment on remove picture', function () {
+            const action: AnyAction = {
+                type: removePicture.fulfilled,
+                payload: {...UserStub(IUserRole.USER, "01"), picture: undefined}
+            }
+            const state = likedPostsReducer({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {
+                                ...PostStub(),
+                                comments: [CommentStub(), {
+                                    ...CommentStub(),
+                                    commenter: {...CommentStub().commenter, _id: "01", picture: "link"}
+                                }]
+                            },
+                        ], userId: "11"
+                    }
+                ]
+            }, action)
+            expect(state).toEqual({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {
+                                ...PostStub(),
+                                comments: [CommentStub(), {
+                                    ...CommentStub(),
+                                    commenter: {...CommentStub().commenter, _id: "01", picture: undefined}
+                                }]
+                            },
+                        ], userId: "11"
+                    }
+                ]
+            })
+        });
+
+    });
+
+    describe('uploadPicture', function () {
+
+        it('should update the posts comment on upload picture', function () {
+            const action: AnyAction = {
+                type: uploadPicture.fulfilled,
+                payload: {_id: "01", picture: "link"}
+            }
+            const state = likedPostsReducer({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {
+                                ...PostStub(),
+                                comments: [CommentStub(), {
+                                    ...CommentStub(),
+                                    commenter: {...CommentStub().commenter, _id: "01", picture: undefined}
+                                }]
+                            },
+                        ], userId: "11"
+                    }
+                ]
+            }, action)
+            expect(state).toEqual({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {
+                                ...PostStub(),
+                                comments: [CommentStub(), {
+                                    ...CommentStub(),
+                                    commenter: {...CommentStub().commenter, _id: "01", picture: "link"}
+                                }]
+                            },
+                        ], userId: "11"
+                    }
+                ]
+            })
+        });
+
+    });
+
+    describe('updateUser', function () {
+
+        it('should update the posts comment on upload picture', function () {
+            const action: AnyAction = {
+                type: updateUser.fulfilled,
+                payload: {...UserStub(IUserRole.USER, "01"), pseudo: "newPseudo"}
+            }
+            const state = likedPostsReducer({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {
+                                ...PostStub(),
+                                comments: [CommentStub(), {
+                                    ...CommentStub(),
+                                    commenter: {...CommentStub().commenter, _id: "01", pseudo: "oldPseudo"}
+                                }]
+                            },
+                        ], userId: "11"
+                    }
+                ]
+            }, action)
+            expect(state).toEqual({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {
+                                ...PostStub(),
+                                comments: [CommentStub(), {
+                                    ...CommentStub(),
+                                    commenter: {...CommentStub().commenter, _id: "01", pseudo: "newPseudo"}
+                                }]
+                            },
+                        ], userId: "11"
+                    }
+                ]
+            })
+        });
+
+    });
+
+    describe('deleteAccount', function () {
+
+        it('should filter the posts and comment on delete account', function () {
+            const action: AnyAction = {
+                type: deleteAccount.fulfilled,
+                payload: UserStub(IUserRole.USER, "01")
+            }
+            const state = likedPostsReducer({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {
+                                ...PostStub(),
+                                comments: [CommentStub(), {
+                                    ...CommentStub(),
+                                    commenter: {...CommentStub().commenter, _id: "01"}
+                                }]
+                            },
+                        ], userId: "11"
+                    }, {
+                        posts: [], userId: "01"
+                    }
+                ]
+            }, action)
+            expect(state).toEqual({
+                ...initialState,
+                users: [
+                    {
+                        posts: [
+                            {
+                                ...PostStub(),
+                                comments: [CommentStub()]
+                            },
+                        ], userId: "11"
+                    }
+                ]
             })
         });
 
